b12c82f3da079c86aa76828fa6011f333beb2263
kpilszak <klaudia.pilszak@gmail.com>
2018-11-21 16:31:37 +0100

    6 colors of squares

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -1,1 +1,14 @@
-alert("JS CONNECTED");
+var colors = [
+    "rgb(0, 0, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 0, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(255, 255, 255)"
+];
+
+var squares = document.getElementsByClassName("square");
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+}
