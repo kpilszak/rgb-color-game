ec065d81b11552d321c51d59bc5d632880cc0e28
kpilszak <klaudia.pilszak@gmail.com>
2018-11-21 17:27:03 +0100

    Change other colors to pickedColor when guessed

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -19,9 +19,18 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor) {
             message.textContent = "You are right!";
+            adjustColors(clickedColor);
         } else {
             this.style.backgroundColor = "rgb(237, 234, 232)";
             message.textContent = "Try again";
         }
     });
+}
+
+function adjustColors(color) {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+    var siteTitle = document.getElementById("siteTitle");
+    siteTitle.style.backgroundColor = color;
 }
