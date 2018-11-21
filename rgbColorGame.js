f894891997428d9c758e45a0535d2854bd93b3e5
kpilszak <klaudia.pilszak@gmail.com>
2018-11-21 16:48:20 +0100

    Change colorDisplay

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -9,6 +9,8 @@ var colors = [
 
 var squares = document.getElementsByClassName("square");
 var pickedColor = colors[2];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
