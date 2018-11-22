2b640cd2521a2ff7a046865a570b2c9e7aa7ece4
kpilszak <klaudia.pilszak@gmail.com>
2018-11-22 14:29:10 +0100

    Add easy/hard mode

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -1,4 +1,5 @@
-var colors = randomizeColors(6);
+var numOfSquares = 6;
+var colors = randomizeColors(numOfSquares);
 var squares = document.getElementsByClassName("square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -6,8 +7,13 @@ colorDisplay.textContent = pickedColor;
 var siteTitle = document.getElementById("siteTitle");
 var message = document.getElementById("message");
 var newGameButton = document.getElementById("newGame");
+var easyButton = document.getElementById("easy");
+var hardButton = document.getElementById("hard");
+
+
 newGameButton.addEventListener("click", function(){
-    colors = randomizeColors(6);
+    newGameButton.style.margin = "2px 120px 0px auto";
+    colors = randomizeColors(numOfSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (var i = 0; i < squares.length; i++) {
@@ -17,9 +23,39 @@ newGameButton.addEventListener("click", function(){
     message.textContent = "";
 });
 
+easyButton.addEventListener("click", function(){
+    easyButton.classList.add("modeOn");
+    hardButton.classList.remove("modeOn");
+    numOfSquares = 3;
+    colors = randomizeColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("modeOn");
+    hardButton.classList.add("modeOn");
+    numOfSquares = 6;
+    colors = randomizeColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
+        newGameButton.style.margin = "2px 10px 0px auto";
         var clickedColor = this.style.backgroundColor;
         if(clickedColor === pickedColor) {
             message.textContent = "You are right!";
